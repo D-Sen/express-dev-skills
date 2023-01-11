ab1ca8c3e99bdbcfce4d994ac9f5e6ed21b23474
Domonic S <dsen@mail.com>
2023-01-11 15:04:21 -0800

    Index functionality completed

---
+++ models/skill.js
@@ -0,0 +1,13 @@
+ const skills = [
+   {id: 1, skill: 'HTML programming', years: 8},
+   {id: 2, skill: 'JS programming', years: 3},
+   {id: 3, skill: 'SQL queries', years: 3}
+ ];
+ 
+ module.exports = {
+   getAll
+ };
+ 
+ function getAll() {
+   return skills;
+ }
